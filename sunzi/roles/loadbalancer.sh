c4bf4c88ba7031e8ce4fa6515249df417a0a8a26
Tomas Olivares <tolivares@gmail.com>
2014-08-29 10:11:46 -0400

    Modified load balancer role with typos

--- sunzi/roles/loadbalancer.sh
+++ sunzi/roles/loadbalancer.sh
@@ -1,9 +1,9 @@
 # Install Load Balancer 
 
-if rpm -qa 'piranha'; then
+if rpm -qa 'piranha'| grep -w 'piranha'; then
 	echo 'Load Balancer packages are already installed'
 else
-	echo 'Resilient Storage was not installed, doing so now:'
+	echo 'Load Balancer was not installed, doing so now:'
 	yum-config-manager --enable rhel-lb-for-rhel-6-server-rpms
 	yum repolist
 	yum -y groupinstall 'Load Balancer'
